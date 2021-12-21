1c3b1c8191e34a62e36321159879cd4458a06c8e
xZyph3r <89517786+xZyph3r@users.noreply.github.com>
2021-12-21 15:44:37 +0100

    Update generate_pluginmaster.py

--- generate_pluginmaster.py
+++ generate_pluginmaster.py
@@ -51,18 +51,6 @@ def main():
     # update the LastUpdated field in master
     last_updated()
 
-def fixBadZipfile(zipFile):  
- f = open(zipFile, 'r+b')  
- data = f.read()  
- pos = data.find('\x50\x4b\x05\x06') # End of central directory signature  
- if (pos > 0):  
-     self._log("Trancating file at location " + str(pos + 22)+ ".")  
-     f.seek(pos + 22)   # size of 'ZIP end of central directory record' 
-     f.truncate()  
-     f.close()  
- else:  
-     # raise error, file is truncated  
-
 def extract_manifests():
     manifests = []
 
